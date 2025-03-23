cdf807f9efc26f18659ec724bb0fceee0010d6f2
Indrajeet Kumar <indrajeet.kumar@timesinternet.in>
2025-03-23 21:49:43 +0530

    Vercel deployment issue fix

--- next.config.js
+++ next.config.js
@@ -1,7 +1,5 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   output: "export", // Ensures static export
   distDir: "out",   // Output directory
   basePath: process.env.NODE_ENV === "production" ? "/tictactoe-rewards" : "", // Only use basePath in production
@@ -10,4 +8,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig; 
